1b1218ecb896c415a9ef2941b0e23196a2a2386f
Chathu Abeywickrama <chathu.abey93@gmail.com>
2021-02-24 13:54:05 +0530

    initial commit

---
+++ src/containers/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+
+export default function Dashboard() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
---
+++ src/containers/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+
+export default function LandingPage({ navigation }: { navigation: any }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontStyle: "normal", fontSize: 30, fontWeight: "700" }}>
+        Organize your Tasks
+      </Text>
+
+      <View style={{ marginTop: 20 }}>
+        <Text style={{ fontStyle: "normal", fontSize: 15 }}>
+          Lets Organize your Tasks and create a checklist of tasks
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#4267B2",
+          paddingVertical: 10,
+          paddingHorizontal: 80,
+          marginTop: 130,
+          borderRadius: 30,
+        }}
+        onPress={() => navigation.navigate("Dashboard")}
+      >
+        <Text style={{ fontSize: 20, color: "#FFFFFF", fontWeight: "600" }}>
+          Login
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
